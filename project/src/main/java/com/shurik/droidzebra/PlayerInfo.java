9309d473b511b6c5eddfc1faf081f755602c5769
panstromek <panstromek@seznam.cz>
2018-06-30 23:07:48 +0200

    remove unused field and make PlayerInfo package private

--- project/src/main/java/com/shurik/droidzebra/PlayerInfo.java
+++ project/src/main/java/com/shurik/droidzebra/PlayerInfo.java
@@ -6,11 +6,10 @@ import de.earthlingz.oerszebra.BuildConfig;
  * Created by stefan on 18.03.2018.
  */ // player info
 public class PlayerInfo {
-    public PlayerInfo(int _player, int _skill, int _exact_skill, int _wld_skill, int _player_time, int _increment) {
+    PlayerInfo(int _player, int _skill, int _exact_skill, int _wld_skill, int _player_time, int _increment) {
         if (BuildConfig.DEBUG && !(_player == ZebraEngine.PLAYER_BLACK || _player == ZebraEngine.PLAYER_WHITE || _player == ZebraEngine.PLAYER_ZEBRA)) {
             throw new AssertionError();
         }
-        playerColor = _player;
         skill = _skill;
         exactSolvingSkill = _exact_skill;
         wldSolvingSkill = _wld_skill;
@@ -19,7 +18,6 @@ public class PlayerInfo {
 
     }
 
-    public int playerColor;
     public int skill;
     public int exactSolvingSkill;
     public int wldSolvingSkill;
