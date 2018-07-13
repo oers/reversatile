cec46e7037074e1e9a8fbb43e669e462d0c34505
panstromek <panstromek@seznam.cz>
2018-07-14 00:01:34 +0200

    delete zebra player status setters /on its way to immutability

--- project/src/main/java/com/shurik/droidzebra/ZebraPlayerStatus.java
+++ project/src/main/java/com/shurik/droidzebra/ZebraPlayerStatus.java
@@ -16,32 +16,16 @@ public class ZebraPlayerStatus {
         this.moves = moves;
     }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
-
     public String getTime() {
         return time;
-    }
-
-    public void setEval(float eval) {
-        this.eval = eval;
     }
 
     public float getEval() {
         return eval;
     }
 
-    public void setDiscCount(int discCount) {
-        this.discCount = discCount;
-    }
-
     public int getDiscCount() {
         return discCount;
-    }
-
-    public void setMoves(byte[] moves) {
-        this.moves = moves;
     }
 
     public byte[] getMoves() {
