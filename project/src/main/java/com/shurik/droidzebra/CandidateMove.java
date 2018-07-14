1b497eaf361ad66082e0ae2169882b81f3df905e
panstromek <panstromek@seznam.cz>
2018-07-14 13:06:13 +0200

    make CandidateMove immutable

--- project/src/main/java/com/shurik/droidzebra/CandidateMove.java
+++ project/src/main/java/com/shurik/droidzebra/CandidateMove.java
@@ -4,14 +4,16 @@ package com.shurik.droidzebra;
  * Created by stefan on 18.03.2018.
  */ // candidate move with evals
 public class CandidateMove {
-    public Move mMove;
-    public boolean mHasEval;
-    public String mEvalShort;
-    public boolean mBest;
+    public final Move mMove;
+    public final boolean mHasEval;
+    public final String mEvalShort;
+    public final boolean mBest;
 
     public CandidateMove(Move move) {
         mMove = move;
         mHasEval = false;
+        mEvalShort = null;
+        mBest = false;
     }
 
     public CandidateMove(Move move, String evalShort, String evalLong, boolean best) {
