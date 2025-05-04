9fe5ccf7ca632cc7b8f36a550e5d2c2ea81c530f
oers <stmura+git@gmail.com>
2025-05-04 11:58:43 +0200

    Revert "reversatile-79 (#90)"
    This reverts commit 30bc5e097fe5fbcd190ed99592083432446471f3.

--- project/src/androidTest/java/de/earthlingz/oerszebra/BasicTest.java
+++ project/src/androidTest/java/de/earthlingz/oerszebra/BasicTest.java
@@ -1,7 +1,5 @@
 package de.earthlingz.oerszebra;
 
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -24,7 +22,6 @@ class BasicTest {
         while (zebra == null && !zebra.initialized()) {
             Thread.sleep(100);
         }
-        getInstrumentation().waitForIdleSync();
     }
 
     void waitForOpenendDialogs(boolean dismiss) throws InterruptedException {
